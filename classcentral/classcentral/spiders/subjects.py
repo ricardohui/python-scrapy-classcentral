56aa43843ed4a5c84b7bd162c13b621d57ed21bb
Ricardo <eehaha@gmail.com>
2018-11-12 22:30:30 +0800

    completed tutorial

--- classcentral/classcentral/spiders/subjects.py
+++ classcentral/classcentral/spiders/subjects.py
@@ -1,11 +1,44 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
 
 
 class SubjectsSpider(scrapy.Spider):
     name = 'subjects'
     allowed_domains = ['class-central.com']
-    start_urls = ['http://class-central.com/']
+    start_urls = ['http://class-central.com/subjects']
+
+    def __init__(self, subject=None):
+        self.subject = subject
 
     def parse(self, response):
-        pass
+        if self.subject:
+            subject_url = response.xpath(
+                '//a[contains(@title,"'+self.subject+'")]').extract_first()
+            yield Request(response.urljoin(subject_url), callback=self.parse_subject)
+        else:
+            self.logger.info('Scraping all subjects.')
+            subjects = response.xpath(
+                '//*[@class="show-all-subjects view-all-courses"]/@href').extract()
+            for subject in subjects:
+                yield Request(response.urljoin(subject), callback=self.parse_subject)
+
+    def parse_subject(self, response):
+        subject_name = response.xpath('//title/text()').extract_first()
+        subject_name = subject_name.split(' | ')
+        subject_name = subject_name[0]
+        courses = response.xpath('//*[@class="course_name"]')
+        for course in courses:
+            course_name = course.xpath('.//@title')/extract_first()
+            course_url = course.xpath('.//@href').extract_first()
+            absolute_course_url = response.urljoin(course_url)
+
+            yield{
+                'subject_name': subject_name,
+                'course_name': course_name,
+                'absolute_course_url': absolute_course_url
+            }
+
+        next_page = response.xpath('//*[@rel="next"]/@href').extract_first()
+        absolute_next_page = respones.urljoin(next_page)
+        yield Request(absolute_next_page, callback=self.parse_subject)
